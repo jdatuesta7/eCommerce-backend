cf34b42c8bc3925e306740651a65ed5a32610e87
Palaciodiego008 <palaciodiego099@gmail.com>
2021-10-27 20:02:15 -0500

    funcion y api filtros de clientes

--- controllers/ClienteController.js
+++ controllers/ClienteController.js
@@ -58,7 +58,13 @@ const login_cliente = async function (req, res) {
     }
 }
 
+const listar_clientes_filtro_admin = async function (req, res) { 
+    let reg = await Cliente.find();
+    res.status(200).send({data:reg});
+ }
+
 module.exports = {
     registro_cliente,
-    login_cliente
+    login_cliente,
+    listar_clientes_filtro_admin
 }
--- routes/cliente.js
+++ routes/cliente.js
@@ -8,4 +8,6 @@ var api = express.Router();
 api.post('/registro_cliente', clienteController.registro_cliente);
 api.post('/login_cliente', clienteController.login_cliente);
 
+api.get('/listar_clientes_filtro_admin',clienteController.listar_clientes_filtro_admin);
+
 module.exports = api;
