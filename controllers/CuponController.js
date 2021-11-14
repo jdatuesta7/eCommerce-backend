1ae92f0f3eb93bed62d557ab611199ea8c0c4820
Jhonatan <jdatuesta7@gmail.com>
2021-11-14 15:50:03 -0500

    listar y filtrar cupones

--- controllers/CuponController.js
+++ controllers/CuponController.js
@@ -17,6 +17,25 @@ const registro_cupon_admin = async function (req, res) {
     }
 }
 
+const listar_cupones_admin = async function (req, res) {
+    if(req.user){
+        if(req.user.role == 'admin' ){
+
+            let filtro = req.params['filtro'];
+
+            let cupones = await Cupon.find({codigo: new RegExp(filtro, 'i')});
+
+            res.status(200).send({data: cupones});
+
+        }else{
+            res.status(500).send({message: 'UnauthorizedAccess'});
+        }
+    }else{
+        res.status(500).send({message: 'UnauthorizedAccess'});
+    }
+}
+
 module.exports = {
-    registro_cupon_admin
+    registro_cupon_admin,
+    listar_cupones_admin
 }
--- routes/cupon.js
+++ routes/cupon.js
@@ -7,5 +7,6 @@ var api = express.Router();
 var auth = require('../middleware/authenticate');
 
 api.post('/registro_cupon_admin', auth.auth, cuponController.registro_cupon_admin);
+api.get('/listar_cupones_admin/:filtro?', auth.auth, cuponController.listar_cupones_admin);
 
 module.exports = api;
